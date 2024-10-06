0cc1793efc354676faabdc78188c8559d33de4c9
thekightkeeper1 <ty.singh828@gmail.com>
2024-10-05 22:54:23 -0400

    Finish template

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 templates = require('./embeds')
-
-const {Client, IntentsBitField, REST, Routes, ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
-const {transform, getFonts, revertTransform} = require('convert-unicode-fonts');
+const { isValidUrl, convertOptionsToObject } = require("./utils");
+const {Client, IntentsBitField, REST } = require('discord.js');
+const { transform, getFonts } = require('convert-unicode-fonts');
 
 
 const rest = new REST({ version: "10" }).setToken(
@@ -31,6 +31,8 @@ client.on('interactionCreate', (interaction) => {
     if(!interaction.isChatInputCommand) return;
 
     if(interaction.commandName === 'font'){
+        // Boo bad boring command
+
         const font = interaction.options.get('fonttype').value;       
         const word = interaction.options.get('text').value;
         const fonts = getFonts();
@@ -38,15 +40,24 @@ client.on('interactionCreate', (interaction) => {
         interaction.reply(s);
         return;
     } if (interaction.commandName === 'embed_gen') {
-        const template = interaction.options.get('template').value
-
-        // parse options
-        console.log(interaction.options);
-        embed = templates[template](Array(interaction.options));
-        interaction.reply({ embeds: [embed]})
+        // Oh yeah exciting command
+        console.log(`interaction.options ${interaction.options}`);
+        const template = interaction.options.get("template").value;
+        options = convertOptionsToObject(interaction.options); // For formatting the template
+        if (
+            !isValidUrl(options.title_url) ||
+            !isValidUrl(options.image_url) ||
+            !isValidUrl(options.thumbnail_url)
+        ) {
+        interaction.reply({
+            content: "The url you gave me didn't start with http:// or https://",
+            ephemeral: true
+        });
         return;
-        interaction.reply("worked maybe");
-
-
+    } else {
+        embed = templates[template](options);
+        interaction.reply({ embeds: [embed] });
+        return;
+    }   
     }
-})
+})  
